f2cd445460e83ef645ba896d72ce8f732b6381b9
mohannad-java <75316485+mohannad-java@users.noreply.github.com>
2022-03-08 00:43:28 +0300

    Delete Source directory

--- Source/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $title = "الصفحة الرئيسية";
-    $style = "index.css";
-    include './component/header.php';
-    include './component/navbar.php';
-    include './component/content.php';
-    include './component/footer.php';
-?>
